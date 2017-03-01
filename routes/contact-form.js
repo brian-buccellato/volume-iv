3b71573a6dbf0863c621bac84c513bb9b8ffd4fc
Brian Buccellato <brianjbuccellato@gmail.com>
2017-03-01 12:05:46 -0500

    Added volume iv email as a recipient in contact-form route.  Fixed the stretched heads issue on about page.  Removed extra spaces on gear page. Underlined categories on gear page.

--- routes/contact-form.js
+++ routes/contact-form.js
@@ -2,19 +2,6 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var router = express.Router();
 
-/*var smtpConfig = {
-	service: "gmail",
-	auth: {
-		xoauth2: {
-			user: "brianjbuccellato@gmail.com",
-			clientId: "639155267070-kjlg63s8199135otfmikum2v541bmkmf.apps.googleusercontent.com",
-			clientSecret: "CbCaqSdgUASxZAhs7MBECXe0",
-			refreshToken:  "1/XvkfehZptQiqrV9sfQbjTZtRGTePdDRxU4ZqXavZnjU",
-			accessToken: "ya29.Ci-KA9imr7OMYGAnRfMSDAmqja3xIwI0q9YOyUswN4TVLbl51HRPIB0v6XNlhDvfTA"
-		}
-	}
-};*/
-
 var transport = nodemailer.createTransport();
 
 router.sendMail = function(req, res, next) {
@@ -22,9 +9,9 @@ router.sendMail = function(req, res, next) {
 	var data = req.body;
 	transport.sendMail({
 		from: data.email,
-		to: 'brianjbuccellato@gmail.com',
+		to: 'brianjbuccellato@gmail.com, volumeivrecording@gmail.com',
 		subject: 'Recording Inquiry from: ' + data.firstName + ' ' + data.lastName,
-		text: data.msg
+		text: data.msgd
 	});
 	res.json(data);
 };
